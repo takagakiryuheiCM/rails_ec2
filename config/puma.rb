d146d9cdcc25207fc59a67fc446849a86fbc5143
takagakiryuheiCM <takagaki.ryuhei@classmethod.jp>
2024-01-14 06:33:53 +0900

    fix: pumaの設定修正

--- config/puma.rb
+++ config/puma.rb
@@ -2,11 +2,15 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-#port        ENV.fetch("PORT") { 3000 }
-bind "unix:///var/www/rails_ec2/tmp/sockets/puma.sock"
-
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 plugin :tmp_restart
+
+if ENV.fetch("RAILS_ENV") == "production"
+  # 本番環境ではUNIXソケットを使用
+  bind "unix:///var/www/rails_ec2/tmp/sockets/puma.sock"
+else
+  # 開発環境ではTCPポートを使用
+  port ENV.fetch("PORT") { 3000 }
+end
